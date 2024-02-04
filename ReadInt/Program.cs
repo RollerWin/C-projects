0d241a63c9b604274cfa393abcdcd84ef2914806
RollerWin <rinatyych@mail.ru>
2024-02-04 14:43:47 +0300

    put check in separate method

--- ReadInt/Program.cs
+++ ReadInt/Program.cs
@@ -2,24 +2,32 @@
 {
     static void Main(string[] args)
     {
+        string userInput = Console.ReadLine();
+        int correctNumber = CheckCorrectInput(userInput);
+
+        Console.WriteLine($"Вы ввели число: {correctNumber}");
+    }
+
+    static int CheckCorrectInput(string userInput)
+    {
         bool isCorrect = false;
+        int correctNumber = 0;
 
         while(isCorrect != true)
         {
-            Console.Write("Введите число: ");
-            string userInput = Console.ReadLine();
-
-            if(int.TryParse(userInput, out int correctNumber))
+            if(int.TryParse(userInput, out correctNumber))
             {
                 isCorrect = true;
-                Console.WriteLine($"Ввод успешно преобразован в число {correctNumber}");
             }
             else
             {
                 Console.WriteLine("Некорректный ввод! Попробуйте ещё раз");
                 Console.ReadKey();
                 Console.Clear();
+                userInput = Console.ReadLine();
             }
         }
+
+        return correctNumber;
     }
 }
