71303a919ddd1fc1fc538fb409afbffee3bc3bc6
RollerWin <rinatyych@mail.ru>
2024-02-04 12:22:53 +0300

    added ReadInt soultion

---
+++ ReadInt/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        bool isCorrect = false;
+
+        while(isCorrect != true)
+        {
+            Console.Write("Введите число: ");
+            string userInput = Console.ReadLine();
+
+            if(int.TryParse(userInput, out int correctNumber))
+            {
+                isCorrect = true;
+                Console.WriteLine($"Ввод успешно преобразован в число {correctNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод! Попробуйте ещё раз");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
